0ef18d18357c5e16ee2b3b3824042946c67f8c9e
Katiska <Ahti.Katiska@helsinki.fi>
2015-01-03 17:46:26 +0200

    Morse code recognition works

--- Main.hs
+++ Main.hs
@@ -5,8 +5,47 @@ import Control.Monad.IO.Class
 import Control.Monad
 import Control.Concurrent
 import Data.List
+import Data.List.Split
 
-data Morse = Dot | Dash | Space deriving Show
+data Morse = Dot | Dash | BSpace | LSpace | WSpace deriving (Show, Eq)
+
+threshold = 900 -- Threshold
+samplerate = 10 -- In microseconds, takes sample every n micros 
+cuttime =90 -- Number of silent samples to stop sampling
+cutthreshold = 900
+
+type MorseCode = [Morse]
+
+dict :: [(MorseCode, Char)]
+dict =
+    [([Dot, Dash]             , 'A'),
+     ([Dash, Dot, Dot, Dot]   , 'B'),
+     ([Dash, Dot, Dash, Dot]  , 'C'),
+     ([Dash, Dot, Dot]        , 'D'),
+     ([Dot]                   , 'E'),
+     ([Dot, Dot, Dash, Dot]   , 'F'),
+     ([Dash, Dash, Dot]       , 'G'),
+     ([Dot, Dot, Dot, Dot]    , 'H'),
+     ([Dot, Dot]              , 'I'),
+     ([Dot, Dash, Dash, Dash] , 'J'),
+     ([Dash, Dot, Dash]       , 'K'),
+     ([Dot, Dash, Dot, Dot]   , 'L'),
+     ([Dash, Dash]            , 'M'),
+     ([Dash, Dot]             , 'N'),
+     ([Dash,Dash,Dash]        , 'O'),
+     ([Dot,Dash,Dash,Dot]     , 'P'),
+     ([Dash, Dash, Dot, Dash] , 'Q'),
+     ([Dot, Dash, Dot]        , 'R'),
+     ([Dot, Dot, Dot]         , 'S'),
+     ([Dash]                  , 'T'),
+     ([Dot, Dot, Dash, Dash]  , 'U'),
+     ([Dot, Dot, Dot, Dash]   , 'V'),
+     ([Dot, Dash, Dash]       , 'W'),
+     ([Dash, Dot, Dot, Dash]  , 'X'),
+     ([Dash, Dot, Dash, Dash] , 'Y'),
+     ([Dash, Dash, Dot, Dot]  , 'Z'),
+     ([WSpace]                , ' ')
+     ]
 
 
 main :: IO ()
@@ -15,16 +54,12 @@ main = withNXT "/dev/rfcomm0" logic
           logic = do setInputModeConfirm One SoundDB RawMode
                      liftIO $ threadDelay 1000000
                      liftIO $ print "Started Sampling"
-                     initInVals <- replicateM 200 ((liftIO $ threadDelay 100) >> getInputValues One)
+                     initInVals <- replicateM cuttime ((liftIO $ threadDelay samplerate) >> getInputValues One)
                      let initRawVals = map getRawADValue initInVals
                      values <- gatherData initRawVals
                      let duration = valuesToDuration values
                          morse = durationToMorse duration
-                     --liftIO $ print values
-                     liftIO $ print $ length initInVals
-                     liftIO $ print initRawVals
-                     liftIO $ print $ length duration
-                     --liftIO $ print morse
+                     liftIO $ print (decodeSentence $ reverse morse)
                     -- forever $ liftIO (threadDelay 100) >> ((getInputValues One) >>= liftIO . print . getRawADValue)
 
 getRawADValue :: InputValue -> RawADValue
@@ -32,21 +67,35 @@ getRawADValue (InputValue _ _ _ _ _ x _ _ _) = x
 
 gatherData :: [RawADValue] -> NXT [RawADValue]
 gatherData x
-    | all (>900) ((take 200) x) = do y <- getInputValues One
-                                     let val = getRawADValue y 
-                                     return (val:x)
-    | otherwise = do liftIO $ threadDelay 100
+    | all (>cutthreshold) ((take cuttime) x) = do y <- getInputValues One
+                                                  let val = getRawADValue y 
+                                                  return (val:x)
+    | otherwise = do liftIO $ threadDelay samplerate
                      do y <- getInputValues One
                         let val = getRawADValue y
                         gatherData (val:x)
 
 valuesToDuration :: [RawADValue] -> [Int]
-valuesToDuration x = (map sum . group . map (\x -> if x < 900 then 1 else -1)) x
+valuesToDuration x = (map sum . group . map (\x -> if x < threshold then 1 else -1)) x
 
 durationToMorse :: [Int] -> [Morse]
 durationToMorse x = concat (map durHelper x)
                     where
                       durHelper x
-                          | x `div` 24 >= 3 = [Dash]
-                          | x `div` 24 >= 1 = [Dot]
-                          | otherwise = replicate (abs (x `mod` 24)) Space
+                          | x >= 15 = [Dash]
+                          | x >= 1 = [Dot]
+                          | x <= -40 = [WSpace]
+                          | x <= -20 = [LSpace]
+                          | otherwise = []
+
+decodeLetter :: MorseCode -> Char
+decodeLetter morse = maybe ' ' id $ lookup morse dict
+
+decodeSentence :: MorseCode -> String
+decodeSentence morse = map decodeLetter $ concat $ splitLetters $ splitWords morse
+
+splitLetters :: [MorseCode] -> [[MorseCode]]
+splitLetters morse = map (splitOn [LSpace]) morse
+
+splitWords :: MorseCode -> [MorseCode]
+splitWords morse = split (oneOf [WSpace]) morse
